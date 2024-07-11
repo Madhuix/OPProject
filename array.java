fc40740ebddede8e8f7aef56cdf53ddf821ebe48
Madhuix <Madhu1318m@gmail.com>
2024-07-11 17:38:16 +0530

    second

---
+++ array.java
@@ -0,0 +1,12 @@
+public class array {
+   public static void main(String args[]) {
+     int[] marks = new int[3];
+     marks[0] = 95;
+     marks[1] = 89;
+     marks[2] = 98;
+    for(int i=0;i<3;i++){
+        System.out.println(marks[i]);
+    }
+    }
+    
+}
